97f001489d36bb2a92754adbdaf9e27c7826c15d
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-10-25 00:17:22 -0300

    ABI from lottery to the react project and created the first function that brings the contract manager's account.

--- lottery-react/src/lottery.js
+++ lottery-react/src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const address = '0x50766428F922d0fe1d9442697B1FB8110E58AC70'
+const address = '0x8C7cd22D457f8C19b3bB8DA10E9B7CcE55698E19'
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"SelectWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterLottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address);
